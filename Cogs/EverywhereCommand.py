15d54b30d2cd62b8abd0b661c376d2de21d86ad3
veni-vidi-code <71394987+veni-vidi-code@users.noreply.github.com>
2021-09-24 22:09:41 +0200

    Update EverywhereCommand.py

--- Cogs/EverywhereCommand.py
+++ Cogs/EverywhereCommand.py
@@ -15,11 +15,11 @@ class Everywhere(commands.Cog, name="Available everywhere"):
     @commands.command(name="Github",
                       brief="Gives you the Github link",
                       help="This bot is a clone from a Github repository. Click here to open:"
-                           "\nhttps://github.com/The-Bow-Hunter/VirusTotalDiscordBot")
+                           "\nhttps://github.com/veni-vidi-code/VirusTotalDiscordBot")
     async def github(self, ctx):
         await ctx.reply(
             "This bot is a clone from a Github repository. Click here to open:"
-            "\nhttps://github.com/The-Bow-Hunter/VirusTotalDiscordBot")
+            "\nhttps://github.com/veni-vidi-code/VirusTotalDiscordBot")
 
     @commands.command(name="info")
     async def info(self, ctx):
